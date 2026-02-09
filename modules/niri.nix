de1ab73ba7fb1fac543a4b13dd5c62b0f4f31ad9
Igor Durapov <igor.durapov@gmail.com>
2026-02-10 00:48:04 +0200

    wip

--- modules/niri.nix
+++ modules/niri.nix
@@ -9,5 +9,42 @@
   ];
 
   programs.niri = {
+    settings = {
+      hotkey-overlay.skip-at-startup = false;
+
+      layout = {
+        gaps = 12;
+        center-focused-column = "always";
+        preset-column-widths = [
+          {proportion = 0.33333;}
+          {proportion = 0.5;}
+          {proportion = 0.66667;}
+        ];
+        default-column-width = {
+          proportion = 0.5;
+        };
+
+        focus-ring = {
+          enable = true;
+          width = 2;
+          # active.color = colors.active;
+          # inactive.color = colors.inactive;
+        };
+        border.enable = false;
+        shadow = {
+          enable = true;
+          # color = "${colors.shadow}aa";
+        };
+      };
+
+      overview = {
+        zoom = 0.5;
+      };
+
+      environment = {
+        "NIXOS_OZONE_WL" = "1";
+        "DISPLAY" = ":0";
+      }; # Disable keymap overlay at startup
+    };
   };
 }
